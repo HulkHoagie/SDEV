6c304457dfb89940223d79d054634ec667c54abd
HulkHoagie <64626507+HulkHoagie@users.noreply.github.com>
2025-05-08 01:23:31 -0400

    Add files via upload
    code for odd or even numbers

---
+++ codey code for class.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+number = 0
+
+for number in numbers:
+    if number % 2 == 0:
+        print(number, "is even")
+    else:
+        print(number, "is odd")
+        
